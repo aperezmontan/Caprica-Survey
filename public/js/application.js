170be7dee56eaa6aa4a6a0234b2495bb8b494c52
Nikolai Calder <redlacialokin@gmail.com>
2015-05-08 19:45:00 -0400

    fix merge conflict

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#draftBtn").on('click',createDraftSurvey);
 });
 
+var createDraftSurvey = function(e){
+  e.preventDefault();
 
-var div = document.createElement("div");
-div.innerHTML = "Hello, world!";
-document.body.appendChild(div);
-var body = d3.select("body");
-var div = body.append("div");
-div.html("Hello, world!");
+};
