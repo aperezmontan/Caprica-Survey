dbe824b2c449dacec740052b48b5ee7a5ea6c77e
Ari Perez <aris.a.perez@gmail.com>
2015-05-09 15:26:39 -0400

    Removes HIRB func, was messing with query

--- config/environment.rb
+++ config/environment.rb
@@ -21,15 +21,6 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
-require 'hirb'
-
-Hirb.enable
-
-# Pry.config.print = proc do |output, value|
-#   Hirb::View.view_or_page_output(value) || Pry::DEFAULT_PRINT.call(output, value)
-# end
-# require 'pry'
-
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
